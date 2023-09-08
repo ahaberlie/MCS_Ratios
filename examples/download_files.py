68287d84b7b8d781cbce4454ccc5484d6a7e1186
Alex Haberlie <alex.haberlie@gmail.com>
2023-09-08 12:03:42 -0500

    Revision updates
    Added seasonal examples for supplement

--- examples/download_files.py
+++ examples/download_files.py
@@ -12,8 +12,41 @@ def get_url_paths(url, ext='', params={}):
     parent = [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
     return parent
 
-def download():
-    for ext in ['.nc', '.cpg', '.dbf', '.prj', '.sbn', '.sbx', '.shp', '.xml', '.shx']:
+def download_geog():
+    for ext in ['.cpg', '.dbf', '.prj', '.sbn', '.sbx', '.shp', '.xml', '.shx']:
+
+        url = "https://svrimg.niu.edu/npjcas23/"
+
+        result = get_url_paths(url, ext)
+
+        for url in result:
+
+            fname = url.replace("%20", " ")
+
+            fname = fname.split("/")[-1]
+
+            fname = f"../data/{fname}"
+
+            if not os.path.exists(fname):
+
+                r = requests.get(url)
+                print("Downloaded", fname)
+                with open(fname, 'wb') as f:
+                    f.write(r.content)
+            else:
+                print(fname, "exists")
+    
+    
+def download(period=None):
+    '''
+    Takes a period to download. If None, all files will download
+    If annual, the annual files will download, if a season of
+    DJF, MAM, JJA, or SON, download only that season.
+    '''
+    
+    download_geog()
+    
+    for ext in ['.nc']:
 
         url = "https://svrimg.niu.edu/npjcas23/"
 
@@ -27,11 +60,39 @@ def download():
 
             fname = f"../data/{fname}"
             
-            if not os.path.exists(fname):
+            if period == None:
+            
+                if not os.path.exists(fname):
 
-                r = requests.get(url)
-                print("Downloaded", fname)
-                with open(fname, 'wb') as f:
-                    f.write(r.content)
+                    r = requests.get(url)
+                    print("Downloaded", fname)
+                    with open(fname, 'wb') as f:
+                        f.write(r.content)
+                else:
+                    print(fname, "exists")
+                    
+            elif period.lower() == 'annual':
+                
+                if 'years' in fname:
+                    
+                    if not os.path.exists(fname):
+
+                        r = requests.get(url)
+                        print("Downloaded", fname)
+                        with open(fname, 'wb') as f:
+                            f.write(r.content)
+                    else:
+                        print(fname, "exists")
+                        
             else:
-                print(fname, "exists")
+                
+                if period in fname:
+                    
+                    if not os.path.exists(fname):
+
+                        r = requests.get(url)
+                        print("Downloaded", fname)
+                        with open(fname, 'wb') as f:
+                            f.write(r.content)
+                    else:
+                        print(fname, "exists")
